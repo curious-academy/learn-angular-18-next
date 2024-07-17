4b3a2f8fa5fc712b3ceb0dfc37cf539b21acc8b2
Evan BOISSONNOT <evan@boissonnot.fr>
2024-07-17 15:24:33 +0200

    feat: manage required and form valid

--- projects/gameotheque/src/app/features/games/components/form-video-game/form-video-game.component.ts
+++ projects/gameotheque/src/app/features/games/components/form-video-game/form-video-game.component.ts
@@ -1,19 +1,29 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MAT_DATE_LOCALE, provideNativeDateAdapter} from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { VideoGame } from '../../models/video-game';
+import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
+import 'moment/locale/fr';
 
 @Component({
   selector: 'dtbc-form-video-game',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule],
   templateUrl: './form-video-game.component.html',
-  styleUrl: './form-video-game.component.css'
+  styleUrl: './form-video-game.component.css',
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},
+    provideMomentDateAdapter(),
+  ]
 })
 export class FormVideoGameComponent {
+  inputIcon = signal('sentiment_very_satisfied')
+
   saveItem = output<VideoGame>()
   item: VideoGame = {nom: '', releaseDate: new Date()}
 
